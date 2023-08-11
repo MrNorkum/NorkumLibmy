56bdc5baa961507cd1ed6018760b1c5d89fc1bd7
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 21:18:36 +0300

    Successful

--- ft_memset.c
+++ ft_memset.c
@@ -1,25 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 15:58:53 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/07 10:29:52 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
-	unsigned char	*tmp;
 
 	i = -1;
-	tmp = (unsigned char *)b;
 	while (++i < len)
-		tmp[i] = c;
-	return (tmp);
+		(unsigned char *)b[i] = c;
+	return (b);
 }
