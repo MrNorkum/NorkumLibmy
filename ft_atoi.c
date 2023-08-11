568b287414f6e6adfe4a64b5fc3a7b4fc37bde5d
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 20:28:33 +0300

    Successful

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,17 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 16:17:45 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/04 16:39:16 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
 int	ft_atoi(const char *str)
 {
 	int	r;
@@ -21,16 +7,11 @@ int	ft_atoi(const char *str)
 	d = 1;
 	while (*str == 32 || (9 <= *str && *str <= 13))
 		str++;
+	if (*str == 45)
+		d = -1;
 	if (*str == 43 || *str == 45)
-	{
-		if (*str == 45)
-			d = -1;
 		str++;
-	}
-	while (ft_isdigit(*str))
-	{
-		r = r * 10 + *str - 48;
-		str++;
-	}
+	while (48 <= *str && *str <= 57)
+		r = r * 10 + *str++ - 48;
 	return (r * d);
 }
