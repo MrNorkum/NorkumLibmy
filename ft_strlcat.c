3595300677cc86f2ae0677b6f31aaf0e5060fed4
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 21:32:02 +0300

    Successful

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/04 10:17:02 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/06 19:17:33 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	src_len;
-	size_t	dst_len;
+	size_t	srclen;
+	size_t	dstlen;
 	size_t	i;
 
 	i = -1;
-	src_len = ft_strlen(src);
-	dst_len = ft_strlen(dst);
-	if (dst_len >= dstsize)
-		return (dstsize + src_len);
-	while (src[++i] && i + dst_len < dstsize - 1)
-		dst[dst_len + i] = src[i];
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (dstlen >= dstsize)
+		return (dstsize + srclen);
+	while (src[++i] && i + dstlen < dstsize - 1)
+		dst[dstlen + i] = src[i];
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
 }
