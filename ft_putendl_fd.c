84c2edf78ad72e5eee2b58e5a7d1e73b57b113af
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 21:21:21 +0300

    Successful

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,21 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 17:40:49 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/05 17:42:03 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
+#include <unistd.h>
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd(10, fd);
+	while (*s && write(fd, s++, 1))
+		;
+	write(fd, "\n", 1);
 }
 
 void	ft_putendl(char *s)
