73d3925aa7417d4af610a3a52202134bfc40e198
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 21:23:14 +0300

    Successful

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,21 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 17:37:11 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/05 17:39:38 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
 #include <unistd.h>
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	while (*s && write(fd, s++, 1))
+		;
 }
 
 void	ft_putstr(char *s)
