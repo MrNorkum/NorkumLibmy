33559a694a69ff8d9cc9cc1e3be93e6a5ca6ce63
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 21:35:17 +0300

    Successful

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/05 17:18:29 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/05 17:30:11 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 #include <stdlib.h>
 
@@ -18,7 +6,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*str;
 	size_t	i;
 
-	str = malloc(ft_strlen(s) + 1);
+	str = (char *)malloc(ft_strlen(s) + 1);
 	if (!str)
 		return (0);
 	str[ft_strlen(s)] = '\0';
