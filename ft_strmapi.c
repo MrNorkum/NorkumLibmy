c6b46d470b7bdc65bbeec44ba241395a0c51a99c
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 22:14:58 +0300

    Successful

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*str;
 	size_t	i;
 
+	i = -1;
 	str = (char *)malloc(ft_strlen(s) + 1);
 	if (!str)
 		return (0);
 	str[ft_strlen(s)] = '\0';
-	i = -1;
 	while (s[++i])
 		str[i] = f(i, s[i]);
 	return (str);
