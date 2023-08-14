c59e3e5c33ec8142b443faca6d7d52c89da60ef0
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 22:21:41 +0300

    Successful

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,17 +6,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	n;
 
 	h = -1;
-	n = 0;
-	if (needle[0] == '\0')
-		return ((char *)haystack);
 	while (haystack[++h] && h < len)
 	{
+		n = 0;
 		while (haystack[h + n] == needle[n] && haystack[h + n]
 			&& h + n < len)
 			n++;
-		if (needle[n] == '\0')
+		if (!needle[n])
 			return ((char *)haystack + h);
-		n = 0;
 	}
 	return (0);
 }
