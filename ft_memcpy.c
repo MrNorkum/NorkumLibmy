f60470a7e981808919bfcfd028818414356114ec
Mr. Norkum <kelestayyib@gmail.com>
2023-08-15 03:00:14 +0300

    Successful

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,17 +1,6 @@
-#include <string.h>
+#include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-	char	*d;
-	char	*s;
-
-	i = -1;
-	d = (char *)dst;
-	s = (char *)src;
-	if (!dst && !src)
-		return (0);
-	while (++i < n)
-		d[i] = s[i];
-	return (d);
+    return (ft_memmove(dst, src, n));
 }
