6ec158eb33390f0fb0e57118139a0c420f01c147
Mr. Norkum <kelestayyib@gmail.com>
2023-08-19 15:36:39 +0300

    Successful

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,19 +1,19 @@
-static inline int	my_atoi(const char *str, int sign, int res, int mod)
+static int	my_atoi(const char *str, int sign, int res, int mod)
 {
 	if (((9 <= *str && *str <= 13) || *str == 32) && mod == 0)
 		return (my_atoi(str + 1, 1, 0, 0));
 	if (*str == 43 && mod == 0)
 		return (my_atoi(str + 1, 1, 0, 1));
 	if (*str == 45 && mod == 0)
-	    	return (my_atoi(str + 1, -1, 0, 1));
+		return (my_atoi(str + 1, -1, 0, 1));
 	if ('0' <= *str && *str <= '9')
 		return (my_atoi(str + 1, sign, res * 10 + *str - 48, 1));
 	return (res * sign);
 }
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    return (my_atoi(str, 1, 0, 0));
+	return (my_atoi(str, 1, 0, 0));
 }
 ---------------------------------------------------------------------
 int	ft_atoi(const char *str)
